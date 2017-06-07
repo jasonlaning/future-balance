d33c3a09876eaf448c887add71c08d634305b43c
Jason Laning <jason.laning@gmail.com>
2017-06-06 21:14:05 -0400

    added put, post, and delete endpoints

--- users/router.js
+++ users/router.js
@@ -2,6 +2,7 @@ const {BasicStrategy} = require('passport-http');
 const express = require('express');
 const jsonParser = require('body-parser').json();
 const passport = require('passport');
+const uuid = require('uuid');
 
 const {User} = require('./models');
 
@@ -69,20 +70,20 @@ router.get('/login',
 );
 
 // GET (protected, must be signed-in already and have session cookie)
-router.get('/me', loggedIn, function(req, res, next) {
+router.get('/me', loggedIn, (req, res, next) => {
   	res.json({user: req.user.apiRepr()});
 	}
 );
 
 // GET for user to sign out
-router.get('/logout', function(req, res){
+router.get('/logout', (req, res) => {
 	req.session.destroy(function (err) {
   		res.redirect('/');
   	});
 });
 
 // POST for creating new user account
-router.post('/signup', (req, res) => {
+router.post('/sign-up', (req, res) => {
 	if (!req.body) {
 		return res.status(400).json({message: 'No request body'});
 	}
@@ -146,26 +147,106 @@ router.post('/signup', (req, res) => {
 });
 
 // POST (password protected, must have session cookie) for adding a new adjustment entry
+router.post('/me/adjustment-entry', loggedIn, (req, res) => {
+	if (!req.body) {
+		return res.status(400).json({message: 'No request body'});
+	}
+
+	const requiredFields = ['name', 'type', 'amount', 'periodUnit', 'periodType', 'startDate', 'endDate'];
+
+	for (let i = 0; i < requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if(!(field in req.body)) {
+			return res.status(422).json({message: `Missing field: ${field}`});
+		};
+	}
+
+	let {name, type, amount, periodUnit, periodType, startDate, endDate} = req.body;
+
+	if (typeof name !== 'string') {
+		return res.status(422).json({message: 'Incorrect field type: name'});
+	}
+
+	name = name.trim();
+
+	if (name ==='') {
+		return res.status(422).json({message: 'Incorrect field length: name'});
+	}
+
+	if (typeof amount !== 'number') {
+		return res.status(422).json({message: 'Incorrect field type: amount'});
+	}
+
+	if (typeof periodUnit !== 'number') {
+		return res.status(422).json({message: 'Incorrect field type: amount'});
+	}
+
+	//return User.adjustmentEntries.push(req.body);
+	const newAdjustment = {
+		id: uuid.v1(),
+		name: req.body.name,
+		type: req.body.type,
+		amount: req.body.amount,
+		periodUnit: req.body.periodUnit,
+		periodType: req.body.periodType,
+		startDate: req.body.startDate,
+		endDate: req.body.endDate
+	}
+
+	return User
+		.update({username: req.user.username}, {$push: {adjustmentEntries: newAdjustment}})
+		.then(user => res.status(204).json({user: req.user.apiRepr()}).end())
+		.catch(err => res.status(500).json({message: 'Internal server error'}));
+});
 
 // PUT (password protected, must have session cookie) for editing user data 
-// includes: account info, most recent balance, or adjustment entries
+// includes: account info or most recent balance
+router.put('/me', loggedIn, (req, res) => {
+
+	const updated = {};
+	const updateableFields = ['firstName', 'lastName', 'password', 'mostRecentBalance'];
+	updateableFields.forEach(field => {
+		if (field in req.body) {
+			updated[field] = req.body[field];
+		}
+	});
+
+	User
+		.findByIdAndUpdate(req.user.id, {$set: updated}, {new: true})
+		.then(updatedUser => res.status(201).json(req.updatedUser.apiRepr()))
+		.catch(err => res.status(500).json({message: 'Error, update failed'}));
+});
+
+// PUT for editing adjustment entries, cookie required
+router.put('/me/adjustment-entry', loggedIn, (req, res) => {
+
+	User
+		.update({username: req.user.username, 'adjustmentEntries.id': req.body.id}, 
+				{$set: {'adjustmentEntries.$': req.body}}, {new: true})
+		.then(message => res.status(201).json(message).end())
+		.catch(err => res.status(500));
+});
 
 // DELETE user account (password protected, must have session cookie)
 router.delete('/me', loggedIn, (req, res) => {
 	User
-		.findByIdAndRemove(req.params.id)
+		.findByIdAndRemove(req.user.id)
 		.exec()
-		.then(user => res.status(204).end())
+		.then(user => res.status(204).json({redirect: '/'}).end())
 		.catch(err => res.status(500).json({message: 'Internal server error'}));
 });
 
 // DELETE adjustment entry (password protected)
-router.delete('/me/adjustmentEntry', loggedIn, (req, res) => {
-	User.adjustmentEntries
-		.findByIdAndRemove(req.params.id)
-		.exec()
+router.delete('/me/adjustment-entry', loggedIn, (req, res) => {
+	User
+		.update({username: req.user.username}, 
+				{$pull: {adjustmentEntries: {id: req.body.id}}})
 		.then(user => res.status(204).end())
 		.catch(err => res.status(500).json({message: 'Internal server error'}));
 })
+
+router.use('*', function(req, res) {
+	res.status(404).json({message: 'Not Found'});
+});
 
 module.exports = {router};
