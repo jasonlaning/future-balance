b020ca614a818909d98e698c346e5873de001510
Jason Laning <jason.laning@gmail.com>
2017-06-15 16:24:43 -0400

    added functionality to populate unique demo user

--- public/sign-up.js
+++ public/sign-up.js
@@ -22,6 +22,78 @@ function signUpUser(username, password) {
 		})
 }
 
+function randomStr() {
+	return ((Math.random() * 999999999999) + 111111111111).toString();
+}
+
+function populateDemoData() {
+
+	var endDate = '2055-01-01';
+
+	var demoData = {
+		username: 'Demo' + randomStr(),
+		password: 'demo',
+		mostRecentBalance: {
+			date: '2017-06-01',
+			amount: 3000
+		},
+		firstName: 'Demo',
+		adjustmentEntries: [{
+			id: randomStr(),
+			name: 'Rent',
+			type: 'Expense',
+			amount: 2800,
+			periodUnit: 1,
+			periodType: 'month',
+			startDate: moment().date(1),
+			endDate: endDate
+			},{
+			id: randomStr(),
+			name: 'Salary',
+			type: 'Income',
+			amount: 1800,
+			periodUnit: 2,
+			periodType: 'week',
+			startDate: '2017-06-09',
+			endDate: endDate
+			},{
+			id: randomStr(),
+			name: 'Groceries',
+			type: 'Expense',
+			amount: 150,
+			periodUnit: 1,
+			periodType: 'week',
+			startDate: '2017-06-11',
+			endDate: endDate
+			}
+		]};
+
+	return demoData;
+
+}
+
+function createNewDemoUser() {
+
+	var demoData = populateDemoData();
+
+	var settings = {
+	  url: '../users/sign-up',
+	  method: 'POST',
+	  data: JSON.stringify(demoData),
+	  contentType: 'application/json',
+	  dataType: 'json',
+	  error: function(res) {
+	  	var message = res.responseJSON.message;
+	  	$('.js-message').html(message);
+ 		}
+	};
+
+	$.ajax(settings)
+		.done(function (response) {
+			signInDemoUser(demoData.username, demoData.password);
+		})
+}
+
 function signInUser(username, password) {
 
 	var settings = {
@@ -43,18 +115,14 @@ function signInUser(username, password) {
 	});
 }
 
-function postDemoData() {
-	
-}
-
-function getDemoUser() {
+function signInDemoUser(username, password) {
 
 	var settings = {
 	  url: "../users/login",
 	  method: "GET",
 	  headers: {
 	    'content-type': "application/json",
-	    authorization: "Basic " + btoa('demo:demo')
+	    authorization: "Basic " + btoa(username + ':' + password)
 	  }
 	};
 
@@ -73,7 +141,7 @@ function watchDemoClick() {
 	$('.js-demo').click(function(event) {
 		event.preventDefault();
 		event.stopPropagation();
-		getDemoUser();
+		createNewDemoUser();
 	})
 }
 
